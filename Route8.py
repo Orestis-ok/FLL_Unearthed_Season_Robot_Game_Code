3b09be5b8534b0105b4d7c3bcf5e8f30492bd635
Orestis K <okerkines@gmail.com>
2026-06-12 23:35:48 +0300

    Update Route8.py

--- Route8.py
+++ Route8.py
@@ -18,7 +18,7 @@ def Route8():
     rightArm.run_time(-500,500 , then=Stop.BRAKE, wait=False) # Κατεβάζει δεξιό βραχίονα
 
 #### Μ01 "Βούρτσισμα Επιφάνειας" και Μ02 "Αποκάλυψη Χάρτη" ####
-    MoveStraight_Distance(450,500,730,True,True,Stop.BRAKE) # Ευθεία, αποχωρεί από την Βάση, περνάει δίπλα από "Βούρτσισμα Επιφάνειας"
+    MoveStraight_Distance(450,500,710,True,True,Stop.BRAKE) # Ευθεία, αποχωρεί από την Βάση, περνάει δίπλα από "Βούρτσισμα Επιφάνειας"
     PointTurn_Angle(300, 300, -45, True, Stop.BRAKE) # Στροφή αριστερά 45 μοιρών, είναι ΜΠΡΟΣΤΑ από "Αποκάλυψη Χάρτη"
 
     MoveStraight_Distance(200,500,170,True,True,Stop.BRAKE)
@@ -29,6 +29,6 @@ def Route8():
     
 #### Επιστροφή στη Βάση #####
     MoveStraight_Distance(300,500,-150,True,True,Stop.BRAKE) # όπισθεν
-    PointTurn_Angle(300, 300, 42, True, Stop.BRAKE) # Στροφή δεξιά 45 μοιρών
+    PointTurn_Angle(300, 300, 45, True, Stop.BRAKE) # Στροφή δεξιά 45 μοιρών
     MoveSteering_Seconds(-900, 2, 2100)
     wait(300)
