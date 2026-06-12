c79a90fd659c68b07f9977fd9621bf100862c5d8
Orestis K <okerkines@gmail.com>
2026-06-12 23:18:49 +0300

    Update Route7.py

--- Route7.py
+++ Route7.py
@@ -11,23 +11,17 @@ from MyLibrary import *
 ####################### Route program ########################
 def Route7(): 
 
-               
     PortView_Battery()
+
+#### Mission 13 "Ανακατασκευή αγάλματος" ####
+
+    MoveStraight_Distance(400,600, 20,True,True,Stop.COAST) # Λίγο μπροστά
+
+    MoveSteering_Seconds(700, 4, 700) # Φεύγει από τη Βάση με ταχύτητα, σταματάει λίγο πριν το Πλοίο
+    MoveSteering_Seconds(300, 6, 900) # Σπρώχνει το Πλοίο με μικρή ταχύτητα ώστε να Ανυψωθεί
+    leftArm.run_time(-1000, 2000, then=Stop.BRAKE, wait=True) # περιστρέφεται ο μηχανισμός για να αναποδογυρίσει τη Ζυγαριά
+    MoveStraight_Distance(400,600,-100,True,True,Stop.BRAKE) # Μικρή όπισθεν για να τραβήξει τον Μοχλό και να καθαρίσει την Άμμο
+
+    MoveSteering_Seconds(-950, -5, 1200) # Επιστροφή στη Βάση 
+    wait(200)         
     
-    MoveSteering_Seconds(-50, 0, 300) # Μικρή όπισθεν, κολλάει τοίχο
-     
-#### Μ01 "Βούρτσισμα Επιφάνειας" και Μ02 "Αποκάλυψη Χάρτη" ####
-    MoveStraight_Distance(600,700,720,True,True,Stop.BRAKE) # Ευθεία, αποχωρεί από την Βάση, περνάει δίπλα από "Βούρτσισμα Επιφάνειας"
-    PointTurn_Angle(300, 300, -45, True, Stop.BRAKE) # Στροφή αριστερά 45 μοιρών, είναι ΜΠΡΟΣΤΑ από "Αποκάλυψη Χάρτη"
-
-    MoveStraight_Distance(300,500,170,True,True,Stop.BRAKE)
-    rightArm.run_time(300,600 , then=Stop.BRAKE, wait=True) # Ανεβαίνει ο βραχίονας να σηκώσει το "Επιφανειακό χώμα" 
-
-#### Επιστροφή στη Βάση #####
-    MoveStraight_Distance(300,500,-150,True,True,Stop.BRAKE) # όπισθεν
-    PointTurn_Angle(300, 300, 45, True, Stop.BRAKE) # Στροφή δεξιά 45 μοιρών
-    MoveStraight_Distance(600,700,-60,True,True,Stop.BRAKE) # Επιστροφή προς την βάση και ανασήκωση του πινέλου
-    leftArm.run_time(700,900 , then=Stop.BRAKE, wait=True) # Ανεβαίνει ο βραχίονας για να σηκώσει το πινέλο
-    wait(500) # Περιμένει λίγο για να σιγουρευτούμε πως έχει πιάσει το πινέλο
-    leftArm.run_time(-400,800 , then=Stop.BRAKE, wait=True) # Γυρίζει ο βραχίονας προς την άλλη πλευρά με το πινέλο 
-    MoveStraight_Distance(600,700,-720,True,True,Stop.BRAKE) # Επιστροφή Βάση
