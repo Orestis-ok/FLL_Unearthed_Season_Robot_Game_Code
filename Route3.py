938953fc7b918af4371b7d2b44b6e6cf420f52ba
Orestis K <okerkines@gmail.com>
2026-06-12 23:25:01 +0300

    Update Route3.py

--- Route3.py
+++ Route3.py
@@ -13,42 +13,27 @@ def Route3():
 
     PortView_Battery()
 
-### Αποχώρηση από Βάση ####
+    rightArm.run_time(400, 300, then=Stop.BRAKE, wait=False) # Σηκώνεται ψηλά ο μοχλός
     MoveSteering_Seconds(-50, 0, 300) # Μικρή όπισθεν, κολλάει τοίχο
+
+    #### Αποχώρηση από τη Βάση ####
+    MoveStraight_Distance(500,600,270,True,True,Stop.BRAKE) # Φεύγει από τη Βάση
+    PointTurn_Angle(700, 700, -46, True, Stop.BRAKE) # Στροφή αριστερά 46 μοίρες
+    MoveStraight_Distance(500,600,175,True,True,Stop.BRAKE) # Ευθεία, είναι μπροστά από το "Τι είναι προς Πώληση"
+
+    ####  Μ09 "Τι είναι προς Πώληση" ####
+    rightArm.run_angle(-1000, 260, then=Stop.BRAKE, wait=True) #κατεβαίνει μοχλός, κατεβαζει κόκκινο βραχίονα
+    rightArm.run_angle(1000, 100, then=Stop.BRAKE, wait=True) # Επαναφορά μοχλού
     
-    MoveStraight_Distance(500,600,700,True,True,Stop.BRAKE) # Φεύγει από τη Βάση
-    PointTurn_Angle(700, 700, -58, True, Stop.BRAKE) # Στροφή αριστερά 58 μοίρες
-    MoveStraight_Distance(500,750,340,True,True,Stop.BRAKE) # Ευθεία είναι δίπλα στο Σιλό (Μ08)
-    PointTurn_Angle(700, 700, -31, True, Stop.BRAKE) # Στροφή αριστερά 31 μοίρες
-    MoveStraight_Distance(500,750,320,True,True,Stop.BRAKE) # Ευθεία είναι δίπλα από το Ποιος Έζησε Εδώ (Μ05)
-    PointTurn_Angle(800, 700, -138, True, Stop.BRAKE) # Στροφή αριστερά 140 μοίρες, είναι μπροστά από το Τι Είναι Προς Πώληση (Μ09)
+    MoveStraight_Distance(300,600,40,True,True,Stop.BRAKE) # Λίγο Ευθεία, μπαίνει ο μοχλός εντός της Τέντας
+    rightArm.run_time(-300, 800, then=Stop.BRAKE, wait=True) # Κατεβαίνει ο μοχλός και γαντζώνει την Τέντα
+    leftArm.run_angle(-500, 200, then=Stop.BRAKE, wait=True) #κατεβαίνει μοχλός, κατεβαζει κόκκινο βραχίονα
+    wait(200)
+    leftArm.run_angle(500, 200, then=Stop.BRAKE, wait=True) #ανεβαίνει μοχλός επανερχεται στη θέση του
 
-#### Μ09 "Τι είναι προς πώληση" ####
-    MoveStraight_Distance(500,750,150,True,True,Stop.BRAKE) # Ευθεία, πλησιάζει το Τι Είναι Προς Πώληση (Μ09)
-    MoveStraight_Distance(400,250,140,True,True,Stop.COAST) # Ευθεία σπρώχνει με το έμβολο και σηκώνει τη Σκεπή 
-    MoveSteering_Seconds(300, 0, 600) # Ευθεία με steering seconds κάνει έξτρα ευθεία για μεγαλύτερη σιγουριά ότι έχει ανασηκωθεί η τέντα 
-    wait(150)
-    MoveStraight_Distance(600,250,-100,True,True,Stop.BRAKE) # μικρή όπισθεν να βγει το έμβολο από τον Πάγκο της Αγοράς
-    
-## Μ10 "Αναποδογυρίστε τη ζυγαριά" ####    
-    rightArm.run_time(1000, 4500, then=Stop.BRAKE, wait=True) # περιστρέφεται ο μηχανισμός για να αναποδογυρίσει τη Ζυγαριά
-    
-    rightArm.run_time(1000, 7000, then=Stop.BRAKE, wait=False) # περιστρέφεται ο μηχανισμός για να κατεβάσει το έμβολο για τον Δίσκο Ζύγισης 
+    MoveStraight_Distance(300,600,-70,True,True,Stop.BRAKE) # Όπισθεν να σηκώσει την Τέντα
+    MoveStraight_Distance(300,600,30,True,True,Stop.BRAKE) # Λίγο μπροστά να ξεγαντζώσει ο μοχλό
+    rightArm.run_angle(1000, 140, then=Stop.BRAKE, wait=True) # Σηκώνεται λίγο ο γάντζος, είναι στην ευθεία με την Τέντα
+    MoveStraight_Distance(150,600,-100,True,True,Stop.BRAKE) # Όπισθεν, απομάκρυνση από Τέντα
 
-    MoveStraight_Distance(600,250,-100,True,True,Stop.BRAKE) # μικρή όπισθεν, φεύγει από Ζυγαριά
-
-#### Πορεία προς Μ09 "Εμπορεύματα"####
-    PointTurn_Angle(700, 700, -40, True, Stop.BRAKE) # Στροφή αριστερά 40 μοίρες
-    # rightArm.run_time(1000, 6900, then=Stop.BRAKE, wait=False) # περιστρέφεται ο μηχανισμός για να κατεβάσει το έμβολο για τον Δίσκο Ζύγισης 
-    MoveStraight_Distance(800,700,420,True,True,Stop.BRAKE) # Ευθεία, είναι δίπλα από το Ποιος Έζησε Εδώ (Μ05)
-    PointTurn_Angle(700, 700, 50, True, Stop.BRAKE) # Στροφή δεξιά 50 μοίρες
-    MoveStraight_Distance(800,700,310,True,True,Stop.BRAKE) # Ευθεία είναι μπροστά από Σιλό (Μ08)
-    PointTurn_Angle(700, 700, 82, True, Stop.BRAKE) # Στροφή δεξιά 83 μοίρες
-
-#### Μ09 "Εμπορεύματα"####
-    
-    MoveStraight_Distance(600,700,460,True,True,Stop.BRAKE) # Ευθεία, κατεβάζει τον μοχλό στα Εμπορεύματα
-    MoveSteering_Seconds(-900, 110, 1600) # Όπισθεν, γυρίζει στη Βάση
-
-
-    
+    MoveSteering_Seconds(-900, 50, 950) # Γυρνάει πίσω στη Βάση
