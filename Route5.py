8d209282446b6ce914be3c94978534c5c82f9c6c
Orestis K <okerkines@gmail.com>
2026-06-12 23:52:16 +0300

    Update Route5.py

--- Route5.py
+++ Route5.py
@@ -15,11 +15,11 @@ def Route5():
 
 #### Mission 11 "Αντικείμενα Ψαράδων" ####
     MoveSteering_Seconds(400, -10, 900)
-    leftArm.run_time(500, 1400, then=Stop.BRAKE, wait=False) # Φεύγει από τη Βάση περιστρέφοντας τον τροχό
-    MoveSteering_Seconds(100, -5, 1850)
+    leftArm.run_time(500, 1700, then=Stop.BRAKE, wait=False) # Φεύγει από τη Βάση περιστρέφοντας τον τροχό
+    MoveSteering_Seconds(100, -5, 2000)
    
-    leftArm.run_time(900, 1400, then=Stop.BRAKE, wait=False) # Περιστροφή του τροχού για να σηκωθούν τα Αντικείμενα
-    MoveSteering_Seconds(30, 0, 1200)
+    leftArm.run_time(900, 1600, then=Stop.BRAKE, wait=False) # Περιστροφή του τροχού για να σηκωθούν τα Αντικείμενα
+    MoveSteering_Seconds(30, 0, 1600)
 
     leftArm.run_time(-500, 1800, then=Stop.BRAKE, wait=False) # Φεύγει από Αντικείμενα Ψαράδων περιστρέφοντας τον τροχό
     MoveStraight_Distance(100,300,-200,True,True,Stop.BRAKE) # Αποχώρηση με μικρή ταχύτητα για να μη βρει ο τροχός πάνω στα Αντικείμενα
