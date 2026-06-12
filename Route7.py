f2fcf23322a454a8236e386e7a60a56d49931a9a
Orestis K <okerkines@gmail.com>
2026-06-12 23:25:54 +0300

    Update Route7.py

--- Route7.py
+++ Route7.py
@@ -19,7 +19,7 @@ def Route7():
 
     MoveSteering_Seconds(700, 4, 700) # Φεύγει από τη Βάση με ταχύτητα, σταματάει λίγο πριν το Πλοίο
     MoveSteering_Seconds(300, 6, 900) # Σπρώχνει το Πλοίο με μικρή ταχύτητα ώστε να Ανυψωθεί
-    leftArm.run_time(-1000, 2000, then=Stop.BRAKE, wait=True) # περιστρέφεται ο μηχανισμός για να αναποδογυρίσει τη Ζυγαριά
+    leftArm.run_time(-1000, 2300, then=Stop.BRAKE, wait=True) # περιστρέφεται ο μηχανισμός για να αναποδογυρίσει τη Ζυγαριά
     MoveStraight_Distance(400,600,-100,True,True,Stop.BRAKE) # Μικρή όπισθεν για να τραβήξει τον Μοχλό και να καθαρίσει την Άμμο
 
     MoveSteering_Seconds(-950, 10, 1200) # Επιστροφή στη Βάση 
