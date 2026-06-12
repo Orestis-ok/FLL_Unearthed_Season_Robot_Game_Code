4a9432df818a0fd781f7eccdd578776992492a36
Orestis K <okerkines@gmail.com>
2026-06-12 23:18:22 +0300

    Rename Route4 function to Route5 and refactor code

--- Route5.py
+++ Route5.py
@@ -9,19 +9,18 @@ from MyLibrary import *
 
   
 ######################## Route program ########################
-def Route4():
+def Route5():
 
-
-   PortView_Battery()
+    PortView_Battery()
 
 #### Mission 11 "Αντικείμενα Ψαράδων" ####
-   MoveSteering_Seconds(400, -10, 900)
-   leftArm.run_time(500, 1400, then=Stop.BRAKE, wait=False) # Φεύγει από τη Βάση περιστρέφοντας τον τροχό
-   MoveSteering_Seconds(100, -5, 1850)
+    MoveSteering_Seconds(400, -10, 900)
+    leftArm.run_time(500, 1400, then=Stop.BRAKE, wait=False) # Φεύγει από τη Βάση περιστρέφοντας τον τροχό
+    MoveSteering_Seconds(100, -5, 1850)
    
-   leftArm.run_time(400, 1200, then=Stop.BRAKE, wait=False) # Περιστροφή του τροχού για να σηκωθούν τα Αντικείμενα
-   MoveSteering_Seconds(30, 0, 1000)
+    leftArm.run_time(400, 1200, then=Stop.BRAKE, wait=False) # Περιστροφή του τροχού για να σηκωθούν τα Αντικείμενα
+    MoveSteering_Seconds(30, 0, 1000)
 
-   leftArm.run_time(-500, 1800, then=Stop.BRAKE, wait=False) # Φεύγει από Αντικείμενα Ψαράδων περιστρέφοντας τον τροχό
-   MoveStraight_Distance(100,300,-200,True,True,Stop.BRAKE) # Αποχώρηση με μικρή ταχύτητα για να μη βρει ο τροχός πάνω στα Αντικείμενα
-   MoveStraight_Distance(900,900,-290,True,True,Stop.BRAKE) # Επιστροφή στη Βάση 
+    leftArm.run_time(-500, 1800, then=Stop.BRAKE, wait=False) # Φεύγει από Αντικείμενα Ψαράδων περιστρέφοντας τον τροχό
+    MoveStraight_Distance(100,300,-200,True,True,Stop.BRAKE) # Αποχώρηση με μικρή ταχύτητα για να μη βρει ο τροχός πάνω στα Αντικείμενα
+    MoveStraight_Distance(900,900,-290,True,True,Stop.BRAKE) # Επιστροφή στη Βάση 
