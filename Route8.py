ba1ea9b9914219ba66977ac804136c4697816d23
Orestis K <okerkines@gmail.com>
2026-06-12 23:52:47 +0300

    Update Route8.py

--- Route8.py
+++ Route8.py
@@ -13,22 +13,21 @@ def Route8():
 
     PortView_Battery()
     
-    MoveSteering_Seconds(-50, 0, 300) # Μικρή όπισθεν, κολλάει τοίχο
-    leftArm.run_time(-200,500 , then=Stop.BRAKE, wait=False) # ανεβάζει αριστερό βραχίονα
-    rightArm.run_time(-500,500 , then=Stop.BRAKE, wait=False) # Κατεβάζει δεξιό βραχίονα
 
-#### Μ01 "Βούρτσισμα Επιφάνειας" και Μ02 "Αποκάλυψη Χάρτη" ####
-    MoveStraight_Distance(450,500,710,True,True,Stop.BRAKE) # Ευθεία, αποχωρεί από την Βάση, περνάει δίπλα από "Βούρτσισμα Επιφάνειας"
+    MoveSteering_Seconds(-50, 0, 300) # Μικρή όπισθεν, κολλάει τοίχο
+    rightArm.run_time(-500,500 , then=Stop.BRAKE, wait=False) # Κατεβάζει δεξιό βραχίονα
+    MoveStraight_Distance(450,500,730,True,True,Stop.BRAKE) # Ευθεία, αποχωρεί από την Βάση, περνάει δίπλα από "Βούρτσισμα Επιφάνειας"
     PointTurn_Angle(300, 300, -45, True, Stop.BRAKE) # Στροφή αριστερά 45 μοιρών, είναι ΜΠΡΟΣΤΑ από "Αποκάλυψη Χάρτη"
 
-    MoveStraight_Distance(200,500,170,True,True,Stop.BRAKE)
+    MoveStraight_Distance(200,500,130,True,True,Stop.BRAKE) # Ευθεία μπροστά για να σπρώξει την "Αποκάλυψη Χάρτη"
+    MoveStraight_Distance(40,500,20,True,True,Stop.BRAKE) # Λίγο μπροστά για να σιγουρέψει ότι έχει σπρώξει τελείως
+
     rightArm.run_time(800,800 , then=Stop.BRAKE, wait=True) # Ανεβαίνει ο δεξιός βραχίονας να σηκώσει το "Επιφανειακό χώμα" 
-    leftArm.run_time(900,1000 , then=Stop.BRAKE, wait=False) # Κατεβαίνει ο αριστερός βραχίονας για να πιάσει το πινέλο
-    wait(300) # Περιμένει λίγο για να σιγουρευτούμε πως έχει πιάσει το πινέλο
-    leftArm.run_time(-300,1000 , then=Stop.BRAKE, wait=True)  # Ανεβαίνει ο αριστερός βραχίονας με το πινέλο 
-    
-#### Επιστροφή στη Βάση #####
-    MoveStraight_Distance(300,500,-150,True,True,Stop.BRAKE) # όπισθεν
+    MoveStraight_Distance(300,500,-150,True,True,Stop.BRAKE) # όπισθεν, φεύγει από "Βούρτσισμα Επιφάνειας"
     PointTurn_Angle(300, 300, 45, True, Stop.BRAKE) # Στροφή δεξιά 45 μοιρών
-    MoveSteering_Seconds(-900, 2, 2100)
-    wait(300)
+    MoveStraight_Distance(300,500,-130,True,True,Stop.BRAKE) # Όπισθεν, είναι δίπλα στο "Βούρτσισμα Επιφάνειας" για να πάρει τη Βούρτσα
+    MoveStraight_Distance(300,500,90,True,True,Stop.BRAKE) # Λίγο μπροστά για να ευθυγραμμιστεί με τη Βούρτσα
+
+
+    leftArm.run_time(-1000,1800 , then=Stop.BRAKE, wait=True) # Περιστροφή του μοχλού του αριστερού βραχίονα για να πιάσει τη Βούρτσα
+    MoveSteering_Seconds(-900, 7, 1700) # Όπισθεν με μικρή κλίση δεξιά για να γυρίσει στη Βάση
