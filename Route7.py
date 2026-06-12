5a744598f042b11004e4de58e618f6067dcad8a7
Orestis K <okerkines@gmail.com>
2026-06-12 23:22:29 +0300

    Update Route7.py

--- Route7.py
+++ Route7.py
@@ -14,7 +14,7 @@ def Route7():
     PortView_Battery()
 
 #### Mission 13 "Ανακατασκευή αγάλματος" ####
-
+        
     MoveStraight_Distance(400,600, 20,True,True,Stop.COAST) # Λίγο μπροστά
 
     MoveSteering_Seconds(700, 4, 700) # Φεύγει από τη Βάση με ταχύτητα, σταματάει λίγο πριν το Πλοίο
@@ -22,6 +22,6 @@ def Route7():
     leftArm.run_time(-1000, 2000, then=Stop.BRAKE, wait=True) # περιστρέφεται ο μηχανισμός για να αναποδογυρίσει τη Ζυγαριά
     MoveStraight_Distance(400,600,-100,True,True,Stop.BRAKE) # Μικρή όπισθεν για να τραβήξει τον Μοχλό και να καθαρίσει την Άμμο
 
-    MoveSteering_Seconds(-950, -5, 1200) # Επιστροφή στη Βάση 
+    MoveSteering_Seconds(-950, 10, 1200) # Επιστροφή στη Βάση 
     wait(200)         
     
