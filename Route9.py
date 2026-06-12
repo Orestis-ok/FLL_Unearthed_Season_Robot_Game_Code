1d352f785b05389caf63a16b58ec0af629cb29b6
Orestis K <okerkines@gmail.com>
2026-06-12 23:26:12 +0300

    Update Route9.py

--- Route9.py
+++ Route9.py
@@ -7,28 +7,33 @@ from pybricks.tools import wait, StopWatch, Matrix
 
 from MyLibrary import *
 
-
+  
 ####################### Route program ########################
-def Route8(): 
-
+def Route9(): 
+    
     PortView_Battery()
-    
+
+    #### Αποχώρηση από Βάση ####
     MoveSteering_Seconds(-50, 0, 300) # Μικρή όπισθεν, κολλάει τοίχο
-    leftArm.run_time(-200,500 , then=Stop.BRAKE, wait=False) # ανεβάζει αριστερό βραχίονα
-    rightArm.run_time(-500,500 , then=Stop.BRAKE, wait=False) # Κατεβάζει δεξιό βραχίονα
 
-#### Μ01 "Βούρτσισμα Επιφάνειας" και Μ02 "Αποκάλυψη Χάρτη" ####
-    MoveStraight_Distance(450,500,730,True,True,Stop.BRAKE) # Ευθεία, αποχωρεί από την Βάση, περνάει δίπλα από "Βούρτσισμα Επιφάνειας"
-    PointTurn_Angle(300, 300, -45, True, Stop.BRAKE) # Στροφή αριστερά 45 μοιρών, είναι ΜΠΡΟΣΤΑ από "Αποκάλυψη Χάρτη"
+    MoveStraight_Distance(500,700,820,True,True,Stop.BRAKE) # Ευθεία μέχρι τον Βόρειο τοίχο
+    PointTurn_Angle(500, 300, 187, True, Stop.BRAKE) # Στροφή 187 μοιρών, ετοιμάζεται να βρει τοίχο
+    MoveStraight_Distance(800,700,-290,True,True,Stop.BRAKE) # Πηγαίνει με όπισθεν μέχρι τον Βόρειο τοίχο
+    MoveSteering_Seconds(-50, 0, 300) # Όπισθεν κολλάει τοίχο
+      
+    ##### Προσεκτική αποκατάσταση M04 #####
 
-    MoveStraight_Distance(200,500,170,True,True,Stop.BRAKE)
-    rightArm.run_time(800,800 , then=Stop.BRAKE, wait=False) # Ανεβαίνει ο δεξιός βραχίονας να σηκώσει το "Επιφανειακό χώμα" 
-    leftArm.run_time(800,1000 , then=Stop.BRAKE, wait=False) # Κατεβαίνει ο αριστερός βραχίονας για να πιάσει το πινέλο
-    wait(300) # Περιμένει λίγο για να σιγουρευτούμε πως έχει πιάσει το πινέλο
-    leftArm.run_time(-300,1000 , then=Stop.BRAKE, wait=True)  # Ανεβαίνει ο αριστερός βραχίονας με το πινέλο 
-    
-#### Επιστροφή στη Βάση #####
-    MoveStraight_Distance(300,500,-150,True,True,Stop.BRAKE) # όπισθεν
-    PointTurn_Angle(300, 300, 42, True, Stop.BRAKE) # Στροφή δεξιά 45 μοιρών
-    MoveSteering_Seconds(-900, 10, 1900)
+    MoveStraight_Distance(800,500,70,True,True,Stop.BRAKE) # Ευθεία φεύγει από τοίχο
+    PointTurn_Angle(300, 300, -93, True, Stop.BRAKE) # Στροφή αριστερά ώστε να ευθυγραμμιστεί με το Αντικείμενο
+    wait(200)
+    MoveSteering_Seconds(100, -10, 1200) # ευθεία φτάνει δίπλα στο Αντικείμενο
+
+    leftArm.run_time(-800,1200, then=Stop.BRAKE, wait=False) # Στρίβει ο βραχίονας να πιάσει το Αντικείμενο
+    rightArm.run_time(60, 2200, then=Stop.BRAKE, wait=True) # Ανεβάζει τον δεξί μοχλό για να σηκώσει τη ράμπα
     wait(300)
+    MoveStraight_Distance(100,500,-120,True,True,Stop.BRAKE) # όπισθεν, βγαίνει με το Αντικείμενο
+
+    #### Επιστροφή στη Βάση ####
+    PointTurn_Angle(300, 300, 100, True, Stop.BRAKE) # στροφή δεξιά, η Βάση είναι μπροστά
+    rightArm.run_time(-200, 1000, then=Stop.BRAKE, wait=False)
+    MoveSteering_Seconds(1000, 0, 2000) # Όπισθεν κολλάει τοίχο
