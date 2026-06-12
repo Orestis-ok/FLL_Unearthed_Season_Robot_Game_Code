5a630b3021ae090ef5b245354cba521d79bf5f49
Orestis K <okerkines@gmail.com>
2026-06-12 23:24:41 +0300

    Update Route1.py

--- Route1.py
+++ Route1.py
@@ -12,26 +12,26 @@ def Route1():
 
     PortView_Battery()
 
-#### Αποχώρηση από Βάση ####
+### Αποχώρηση από Βάση ####
     MoveSteering_Seconds(-50, 0, 300) # Μικρή όπισθεν, κολλάει τοίχο
 
 
 #### Μ08 "Σιλό" ####
 
     MoveStraight_Distance(800,500,490,True,True,Stop.BRAKE) # Ευθεία, φεύγει από τη Βάση, είναι δίπλα στο Σιλό
-    rightArm.run_time(-900, 800, then=Stop.BRAKE, wait=True)
+    rightArm.run_time(-900, 600, then=Stop.BRAKE, wait=True)
     wait(300)
-    rightArm.run_time(900, 800, then=Stop.BRAKE, wait=True)
+    rightArm.run_time(900, 600, then=Stop.BRAKE, wait=True)
     wait(300)
-    rightArm.run_time(-900, 800, then=Stop.BRAKE, wait=True) # Συνεχόμενα χτυπήματα στον μοχλό, ΚΑΤΕΒΑΣΜΑ
+    rightArm.run_time(-900, 600, then=Stop.BRAKE, wait=True) # Συνεχόμενα χτυπήματα στον μοχλό, ΚΑΤΕΒΑΣΜΑ
     wait(150)
-    rightArm.run_time(900, 800, then=Stop.BRAKE, wait=True) # Συνεχόμενα χτυπήματα στον μοχλό, ΑΝΕΒΑΣΜΑ
+    rightArm.run_time(900, 600, then=Stop.BRAKE, wait=True) # Συνεχόμενα χτυπήματα στον μοχλό, ΑΝΕΒΑΣΜΑ
     wait(150)
-    rightArm.run_time(-900, 800, then=Stop.BRAKE, wait=True)
+    rightArm.run_time(-900, 600, then=Stop.BRAKE, wait=True)
     wait(150)
-    rightArm.run_time(900, 800, then=Stop.BRAKE, wait=True)
-    wait(150)
-    rightArm.run_time(-900, 800, then=Stop.BRAKE, wait=True)
-    wait(150)
-    rightArm.run_time(900, 800, then=Stop.BRAKE, wait=True)
+    # rightArm.run_time(900, 600, then=Stop.BRAKE, wait=True)
+    # wait(150)
+    # rightArm.run_time(-900, 600, then=Stop.BRAKE, wait=True)
+    # wait(150)
+    rightArm.run_time(900, 600, then=Stop.BRAKE, wait=True)
     MoveStraight_Distance(900,500,-430,True,True,Stop.BRAKE) # Όπισθεν, Επιστρέφει στη Βάση
